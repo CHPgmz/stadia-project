1cde6ba8def2d661af3910345d637c1e47300807
Alfredo <chpgomezito@gmail.com>
2022-01-26 11:33:29 -0600

    Se agrego el menu desplegable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,16 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width" />
-        <title>Example</title>
+        <title>Home</title>
     </head>
     <body>
-        <p>Hola Mundo PerroHola Mundo Perross</p>
+        <p>Hola mundo :)</p>
+        <?php echo 
+            {{ Routes{{ ss 
+            }} }}
+        ?>
+
     </body>
+    <p>ksk
+    </p>
 </html>
