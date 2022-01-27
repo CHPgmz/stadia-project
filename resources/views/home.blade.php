9c8ba6aeb240e020fd34a9b8c53beda76fa0e6c2
Alfredo <chpgomezito@gmail.com>
2022-01-27 14:26:07 -0600

    Se agrego los iconos de los botones de editar/eliminar en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,43 @@
 
 @section('estilos')
 
-<link rel="stylesheet" href="{{ asset('css-table-new/util.css') }}">
+<!--<link rel="stylesheet" href="{{ asset('css-table-new/util.css') }}">-->
 <link rel="stylesheet" href="{{asset('css-table-new/main.css')}}">
 <link rel="stylesheet" href="{{asset('css-table-new/style-status.css')}}">
+<style type="text/css" >
+.button-edit {
+	background: #81d4fa;
+	width: 80px;
+	height: 23px;
+	border-radius: 3px;
+}
+.button-edit > i{
+	margin-right: 4px;
+}
+.button-delete > i{
+	margin-right: 4px;
+}
+.button-delete {
+	background: #e53935;
+	width: 80px;
+	height: 23px;
+	border-radius: 3px;
+}
+
+.name-obra {
+	font-weight: bold;
+	font-size: 12px;
+}
+</style>
 
 @endsection
 
 @section('contenido')
+<div class="options-registers">
+	<a class="register" href="#">Registrar Obra</a>
+	<a class="register" href="#">Resgistrar Tecnico</a>
+</div>
+
 <div class="limiter">
 		<div class="container-table100">
 			<div class="wrap-table100">
@@ -23,8 +53,7 @@
 									<th class="cell100 column4">Supervisor</th>
 									<th class="cell100 column5">Fecha_Inicio</th>
                            <th class="cell100 column6">Fecha-Terminacion</th>
-                           
-                           <th class="cell100 column8">Acciones</th>
+                           <th class="cell100 column7">Acciones</th>
 								</tr>
 							</thead>
 						</table>
@@ -34,84 +63,106 @@
 						<table class="table-body">
 							<tbody>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">DTTO SATURADO PRIN</td>
                            <td class="cell100 column3 status"><span class="active">90% de pago</span></td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">01/01/2022</td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+										<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+									</td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">PVU001 Adipisicing impedit</td>
+									<td class="cell100 column1 name-obra">PVU001 Adipisicing impedit</td>
 									<td class="cell100 column2">PRAL</td>
-                           <td class="cell100 column3 status"><span class="waiting">Proceso 90%</span></td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">03/01/2022</td>
+                           <td class="cell100 column3 status"><span class="waiting">Proceso 60%</span></td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">03/01/2022</td>
 									<td class="cell100 column6">14/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RPZ004 Sit sapiente lorem2</td>
+									<td class="cell100 column1 name-obra">RPZ004 Sit sapiente</td>
 									<td class="cell100 column2">FTTH</td>
                            <td class="cell100 column3 status"><span class="active">Proceso 90%</span></td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">01/01/2022</td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">RDA</td>
-                           <td class="cell100 column3 status"><span class="waiting">Proceso 90%</span></td>
-									<td class="cell100 column3">Hector</td>
-									<td class="cell100 column4">01/01/2022</td>
+                           <td class="cell100 column3 status"><span class="waiting">Proceso 60%</span></td>
+									<td class="cell100 column4">Hector</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">RDA</td>
-                           <td class="cell100 column3">Proceso 90% de pago</td>
+                           <td class="cell100 column3 status"><span class="active">90% de pago</span></td>
 									<td class="cell100 column3">Gilberto</td>
 									<td class="cell100 column4">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column8">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">RDA</td>
-                           <td class="cell100 column3">Proceso 90% de pago</td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">01/01/2022</td>
+                           <td class="cell100 column3 status"><span class="active">90% de pago</span></td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7"><button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button></td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">RDA</td>
-                           <td class="cell100 column3">Proceso 90% de pago</td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">01/01/2022</td>
+                           <td class="cell100 column3 status"><span class="active">90% de pago</span></td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 								<tr class="row100 body">
-									<td class="cell100 column1">RDA Medical Baumachine</td>
+									<td class="cell100 column1 name-obra">RDA Medical Baumachine</td>
 									<td class="cell100 column2">RDA</td>
-                           <td class="cell100 column3">Proceso 90% de pago</td>
-									<td class="cell100 column3">Epifanio</td>
-									<td class="cell100 column4">01/01/2022</td>
+                           <td class="cell100 column3 status"><span class="active">90% de pago</span></td>
+									<td class="cell100 column4">Epifanio</td>
+									<td class="cell100 column5">01/01/2022</td>
 									<td class="cell100 column6">10/02/2022</td>
 
-                           <td class="cell100 column8">editar</td>
+                           <td class="cell100 column7">
+                           	<button class="button-edit" role="button"><i class="far fa-edit"></i>Detalles</button>
+										<button class="button-delete" role="button"><i class="far fa-trash-alt"></i>Eliminar</button>
+                           </td>
 								</tr>
 
                         <!--<tr class="row100 body">
@@ -181,6 +232,6 @@
 @endsection
 
 @section('scripts')
-
+<script src="https://kit.fontawesome.com/2ead8049d9.js" crossorigin="anonymous"></script>
 @endsection
 
